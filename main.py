17451dc5b9ea0fd270e50d2a00b9fcbf3f423a00
gpanelli3 <gpanelli3@gmail.com>
2024-04-01 17:24:01 -0300

    'input-casi'

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from app import Wsgiclass
 
 app=Wsgiclass()
 #waitress-serve --listen=*:8000 main:app
+
+
 @app.ruta("/home")
 def home(request,response):
     conexion= mysql.connector.connect(host='localhost',
@@ -27,11 +29,42 @@ def home(request,response):
 def home(request,response):
 
      response.text= app.template(
-        "productos.html", context={"title": "Pagina Principal", "user": "PRODUCTOS"})
+        "productos.html", context={"title": "Pagina Principal", "user": "PRODUCTOS INGRESADOS CORRECTAMENTE"})
    
 
 
 @app.ruta("/ingresoProd")
 def ingresoProd(request,response):
+
+    conexion= mysql.connector.connect(host='localhost',
+                                  user='genaro',
+                                  passwd='password',
+                                  database='sistema-ventas')
+    
+    cursor=conexion.cursor()
+    
+    #idp=request.POST.get('id')
+    nombre=request.POST.get('nombre')
+    descripcion=request.POST.get('descripcion')
+    precio=request.POST.get('precio')
+    cantidad=request.POST.get('cantidad')
+    categoria=request.POST.get('categoria')
+
+    try:
+        sql="INSERT INTO producto(idProducto,nombre,descripcion,precio,cantidad,id_cat_corresp) VALUES(%s,%s,%s,%s,%s,%s)"
+        datos=(nombre,descripcion,precio,cantidad,categoria)
+        
+        cursor.execute(sql,datos)
+        conexion.commit()
+
+
+    except mysql.connector.Error as error:
+        print("error al actualizar en la base de datos", error)
+
+
+    conexion.close()
+    
+
+
     response.text= app.template(
-        "ingresoProd.html", context={"title": "Ingresar productos al stock","user": "INGRESAR PRODUCTOS"})
+        "ingresoProd.html", context={"user": "PRODUCTOS INGRESADOS CORRECTAMENTE"})
